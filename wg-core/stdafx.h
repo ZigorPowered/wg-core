74f8dd7fb7e4743170b250a2f6b042cb3a6e15fb
Igor B <igorb202@gmail.com>
2018-09-29 12:39:30 -0400

    Created MainWin

--- wg-core/stdafx.h
+++ wg-core/stdafx.h
@@ -1,21 +1,17 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
-
+/*
+stdafx.h
+WGCore precompiled header
+*/
 #pragma once
 
-#include "targetver.h"
-
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#define WIN32_LEAN_AND_MEAN 
 // Windows Header Files:
+#include <SDKDDKVer.h> //Latest Windows
 #include <windows.h>
+
 
 // C RunTime Header Files
 #include <stdlib.h>
 #include <malloc.h>
 #include <memory.h>
 #include <tchar.h>
-
-
-// TODO: reference additional headers your program requires here
--- wg-core/wg-core.cpp
+++ wg-core/wg-core.cpp
@@ -1,180 +1,90 @@
-// wg-core.cpp : Defines the entry point for the application.
-//
-
 #include "stdafx.h"
-#include "wg-core.h"
 
-#define MAX_LOADSTRING 100
+/**********************************************
+FUNC DECLARATIONS
+**********************************************/
+LRESULT CALLBACK MainWndProc(HWND hWnd, UINT msg, WPARAM wPrm, LPARAM lPrm);
+BOOL InitWndCls(LPCTSTR clsName, WNDPROC clsProc);
+VOID SetWndClsStyle(HWND hClsInst, UINT styleFlags, HBRUSH bgBrush, HICON icon, HCURSOR cursor);
 
-// Global Variables:
-HINSTANCE hInst;                                // current instance
-WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
-WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+/**********************************************
+GLOBAL VARIABLES
+**********************************************/
+HINSTANCE hAppInst;
 
-// Forward declarations of functions included in this code module:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
-
+/**********************************************
+ENTRY POINT
+**********************************************/
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
                      _In_ LPWSTR    lpCmdLine,
                      _In_ int       nCmdShow)
 {
+	//Declare Unused Params
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
+	if ((hAppInst = hInstance) == NULL)
+		return -1;
 
-    // TODO: Place code here.
+    MSG appMsg;
+	HWND hMainWin;
 
-    // Initialize global strings
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_WGCORE, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
+	if (!InitWndCls(_T("CLS_MAINWIN"), MainWndProc))
+		return -1;
+	
+	hMainWin = CreateWindowEx(0, _T("CLS_MAINWIN"), _T("WGCore Main"), WS_OVERLAPPEDWINDOW, 0, 0, 1600, 800, 0, 0, hAppInst, 0);
+	if (hMainWin == NULL)
+	{
+		UnregisterClass(_T("CLS_MAINWIN"), hAppInst);
+		return -1;
+	}
+	SetWndClsStyle(hMainWin, CS_OWNDC | CS_HREDRAW | CS_VREDRAW, 0, LoadIcon(0, IDI_APPLICATION), LoadCursor(0, IDC_ARROW));
 
-    // Perform application initialization:
-    if (!InitInstance (hInstance, nCmdShow))
+
+	ShowWindow(hMainWin, SW_SHOW);
+	UpdateWindow(hMainWin);
+    
+    while (GetMessage(&appMsg, nullptr, 0, 0))
     {
-        return FALSE;
+            TranslateMessage(&appMsg);
+            DispatchMessage(&appMsg);
     }
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WGCORE));
+    return (int) appMsg.wParam;
+}
 
-    MSG msg;
+BOOL InitWndCls(LPCTSTR clsName, WNDPROC clsProc)
+{
+	WNDCLASSEX newWndCls;
+	memset(&newWndCls, 0, sizeof(WNDCLASSEX));
+	newWndCls.cbSize = sizeof(WNDCLASSEX);
+	newWndCls.hInstance = hAppInst;
+	newWndCls.lpszClassName = clsName;
+	newWndCls.lpfnWndProc = clsProc;
+	return (RegisterClassEx(&newWndCls) == 0) ? false : true;
+}
+VOID SetWndClsStyle(HWND hClsInst, UINT styleFlags, HBRUSH bgBrush, HICON icon, HCURSOR cursor)
+{
+	SetClassLong(hClsInst, GCL_STYLE, (LONG)styleFlags);
+	SetClassLong(hClsInst, GCLP_HBRBACKGROUND, (LONG)bgBrush);
+	SetClassLong(hClsInst, GCLP_HICON, (LONG)icon);
+	SetClassLong(hClsInst, GCL_HCURSOR, (LONG)cursor);
+}
 
-    // Main message loop:
-    while (GetMessage(&msg, nullptr, 0, 0))
+
+
+/**********************************************
+MainWin Proc
+**********************************************/
+LRESULT CALLBACK MainWndProc(HWND hWnd, UINT msg, WPARAM wPrm, LPARAM lPrm)
+{
+    switch (msg)
     {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
-
-    return (int) msg.wParam;
-}
-
-
-
-//
-//  FUNCTION: MyRegisterClass()
-//
-//  PURPOSE: Registers the window class.
-//
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-    WNDCLASSEXW wcex;
-
-    wcex.cbSize = sizeof(WNDCLASSEX);
-
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WGCORE));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_WGCORE);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-    return RegisterClassExW(&wcex);
-}
-
-//
-//   FUNCTION: InitInstance(HINSTANCE, int)
-//
-//   PURPOSE: Saves instance handle and creates main window
-//
-//   COMMENTS:
-//
-//        In this function, we save the instance handle in a global variable and
-//        create and display the main program window.
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-   hInst = hInstance; // Store instance handle in our global variable
-
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-
-   if (!hWnd)
-   {
-      return FALSE;
-   }
-
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   return TRUE;
-}
-
-//
-//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  PURPOSE:  Processes messages for the main window.
-//
-//  WM_COMMAND  - process the application menu
-//  WM_PAINT    - Paint the main window
-//  WM_DESTROY  - post a quit message and return
-//
-//
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // Parse the menu selections:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Add any drawing code that uses hdc here...
-            EndPaint(hWnd, &ps);
-        }
-        break;
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
     default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
+        return DefWindowProc(hWnd, msg, wPrm, lPrm);
     }
     return 0;
-}
-
-// Message handler for about box.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
 }
