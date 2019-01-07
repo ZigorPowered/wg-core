350b7222e5a53768cb5b16e8fa196363bf6e8c3f
barinovi <igorb202@gmail.com>
2019-01-07 16:16:16 -0500

    Dialog Mehods

--- wg-core/main.cpp
+++ wg-core/main.cpp
@@ -1,19 +1,35 @@
 #include "stdafx.h"
 #include "wg_core.h"
 
-int main(int argc, char* argv[])
+WGCptr64 QuitWindow(WGCdlg_h winHdl, WGCFlag32 quitFlags, WGCptr32 arg1 = WGC_UNUSED_PARAM, WGCptr64 arg2 = WGC_UNUSED_PARAM)
 {
-	CoreAppHdl appHdl = core::GetAppHandle();
-	CoreDlgProc mainWinProc;
-	CoreDlgContext mainWinContext(L"CONTEXT_MAIN_WIN", 
-									&mainWinProc,
-									appHdl,
-									0,
-									CreateSolidBrush(RGB(100,100,100)),
-									LoadCursor(0, IDC_ARROW),
-									LoadIcon(0, IDI_APPLICATION));
+	DestroyWindow(winHdl);
+	PostQuitMessage(0);
+	return WGC_CALLBACK_RESULT(0);
+}
+
+int __stdcall WinMain(HINSTANCE hCurrent, HINSTANCE hPrev, LPSTR cmd, int nCmd)
+{
+	WGCapp_h hMainApp = CORE::GetAppHandle();
+
+	WGCcstr winContextName = WGC_CSTR("WindowContext");
+	CORE::CreateDlgContext(winContextName, hMainApp, CORE::DefaultDlgProc);
+
+	WGCDialog mainWin(winContextName, hMainApp, 100, 100, 800, 600, WGC_CSTR("Main Window"), WS_OVERLAPPEDWINDOW, 0, 0, 0);
+	WGCDialog toolWin(winContextName, hMainApp, 200, 200, 900, 400, WGC_CSTR("Tool Box"), WS_OVERLAPPEDWINDOW, 0, 0, 0);
+
+	WGCDialogProc mainWinProc;
+	mainWinProc.set_callback(WGC_EVENT_CLOSE_DLG, QuitWindow);
+
+	WGCEventWatch appEventWatch;
+	appEventWatch.register_dialog_proc(mainWin.get_handle(), &mainWinProc);
+
+	if (mainWin.start() == WGC_ERROR)
+		return 0;
+
+	while (appEventWatch.is_active())
+		appEventWatch.update();
 
 
-	CoreDlgHdl hMainWin;
 	return 0;
 }
--- wg-core/wg_core.h
+++ wg-core/wg_core.h
@@ -1,48 +1,184 @@
 #pragma once
 
-#define CORE_ERROR -1;
-#define CORE_NO_ERROR 0;
+#define WGC_ERROR			-1
+#define WGC_NO_ERROR		0
+#define WGC_UNUSED_PARAM	0
 
-typedef UINT CoreUINT, CoreFlagU32;
-typedef DWORD CoreULONG, CoreFlagU64;
-typedef LPCTSTR CoreCSTR;
-typedef HINSTANCE CoreAppHdl;
-typedef WNDPROC CoreProc;
-typedef HWND CoreDlgHdl;
-typedef WPARAM CoreProcArgU, CorePtrU;
-typedef LPARAM CoreProcArg, CorePtrL;
+#define WGC_CSTR(s) _T(s)
+#define WGC_MAKE_DLG_ID(i) (HMENU)i
+#define WGC_CALLBACK_RESULT(n) (LRESULT)n
 
-typedef LRESULT(*CoreCallback)(CoreDlgHdl, CorePtrU, CorePtrL);
-typedef std::unordered_map<CoreFlagU32, CoreCallback> CoreProcMap;
+#define WGC_EVENT_QUIT			WM_QUIT
+#define WGC_EVENT_CLOSE_DLG		WM_CLOSE
+#define WGC_EVENT_PROC_USED		WM_USER + 1
 
-class CoreDlgProc;
-class CoreDlgContext;
 
-typedef CoreProc(CoreDlgProc::* CoreDlgProcHdl)();
+typedef UINT WGCuint, WGCFlag32;
+typedef LPCTSTR WGCcstr;
+typedef DWORD WGCFlag64;
+typedef WPARAM WGCptr_u, WGCptr32;
+typedef LPARAM WGCptr_l, WGCptr64;
 
-class CoreDlgProc
+typedef HINSTANCE WGCapp_h;
+typedef WNDPROC WGCproc;
+typedef HWND WGCdlg_h;
+typedef HMENU WGCdlg_id;
+
+typedef HBRUSH WGCpalette_h;
+typedef HICON WGCicon_rc;
+typedef HCURSOR WGCcursor_rc;
+
+typedef WGCptr64(*WGCcallback)(WGCdlg_h, WGCFlag32, WGCptr32, WGCptr64);
+
+class WGCDlgProc;
+class WGCDialog;
+class WGCEventWatch;
+
+namespace CORE
 {
-public:
-	CoreDlgProc();
-	~CoreDlgProc();
+	WGCapp_h GetAppHandle()
+	{
+		return GetModuleHandle(0);
+	}
+	WGCptr64 CALLBACK DefaultDlgProc(WGCdlg_h dialogHdl, WGCFlag32 eventFlags, WGCptr32 uArgPtr, WGCptr64 lArgPtr)
+	{
+		if (eventFlags == WGC_EVENT_PROC_USED)
+			return (WGCptr64)WGC_NO_ERROR;
 
-	void set_callback(CoreFlagU32 callbackID, CoreCallback callback);
-	void remove_callback(CoreFlagU32 callbackID);
-	CoreDlgProcHdl get_proc_handle();
-private:
-	CoreProcMap* _callbacks;
-	LRESULT _proc(CoreDlgHdl hDlg, CoreFlagU32 msgFlags, CorePtrU wPrm, CorePtrL lPrm);
-};
-
-namespace core
-{
-	CoreAppHdl GetAppHandle();
-	LRESULT __stdcall DefaultDialogProc(CoreDlgHdl hDialog, CoreFlagU32 procFlags, CoreProcArgU uArgs, CoreProcArg args);
-	int CreateDlgContext(CoreCSTR contextName, CoreDlgProc* contextProc, CoreAppHdl appHdl, CoreFlagU32 contextFlags = 0, HBRUSH bgData = 0, HCURSOR cursorData = 0, HICON iconData = 0);
-	CoreDlgHdl CreateDlg(HINSTANCE appHdl, CoreCSTR dlgContextName, CoreCSTR dlgTitle, int xPos, int yPos, int width, int height, CoreFlagU64 dlgStyle, HMENU dlgID = 0, HWND hParentDlg = 0);
+		return DefWindowProc(dialogHdl, eventFlags, uArgPtr, lArgPtr);
+	}
+	int CreateDlgContext(WGCcstr contextName, WGCapp_h appHdl, WGCproc dlgProc, WGCFlag32 contextFlags = 0U, WGCpalette_h bgPalette = NULL, WGCcursor_rc cursorType = LoadCursor(0, IDC_ARROW), WGCicon_rc iconType = LoadIcon(0, IDI_APPLICATION))
+	{
+		WNDCLASSEX contextData;
+		memset(&contextData, 0, sizeof(WNDCLASSEX));
+		contextData.cbSize = sizeof(WNDCLASSEX);
+		contextData.lpszClassName = contextName;
+		contextData.hInstance = appHdl;
+		contextData.lpfnWndProc = dlgProc;
+		contextData.style = contextFlags;
+		contextData.hbrBackground = bgPalette;
+		contextData.hCursor = cursorType;
+		contextData.hIcon = iconType;
+		return (RegisterClassEx(&contextData) == 0) ? WGC_ERROR : WGC_NO_ERROR;
+	}
 }
 
+class WGCDialogProc
+{
+public:
+	WGCDialogProc() :
+		_callbackMap(new std::unordered_map<WGCFlag32, WGCcallback>)
+	{
+	}
+	~WGCDialogProc()
+	{
+		delete this->_callbackMap;
+	}
 
+	void set_callback(WGCFlag32 eventFlag, WGCcallback eventCallback)
+	{
+		(*this->_callbackMap)[eventFlag] = eventCallback;
+	}
+	WGCptr64 __stdcall handle_event(WGCdlg_h dialogHdl, WGCFlag32 eventFlags, WGCptr32 uArgPtr, WGCptr64 lArgPtr)
+	{
+		auto mapSearch = _callbackMap->find(eventFlags);
+
+		if (mapSearch == _callbackMap->end())
+			return CORE::DefaultDlgProc(dialogHdl, eventFlags, uArgPtr, lArgPtr);
+
+		return (mapSearch->second)(dialogHdl, eventFlags, uArgPtr, lArgPtr);
+	}
+private:
+	std::unordered_map<WGCFlag32, WGCcallback>* _callbackMap;
+};
+
+class WGCEventWatch
+{
+public:
+	WGCEventWatch() :
+		_eventData(new MSG), _procMap(new std::unordered_map<WGCdlg_h, WGCDialogProc*>)
+	{
+	}
+
+	void register_dialog_proc(WGCdlg_h dialogHdl, WGCDialogProc* dlgProc)
+	{
+		(*this->_procMap)[dialogHdl] = dlgProc;
+	}
+	bool is_active()
+	{
+		return GetMessage(this->_eventData, 0, 0, 0) > 0;
+	}
+	int update()
+	{
+		if (!this->is_active())
+			return WGC_ERROR;
+
+		auto procSearch = _procMap->find(_eventData->hwnd);
+		if (procSearch != _procMap->end())
+		{
+			procSearch->second->handle_event(_eventData->hwnd, _eventData->message, _eventData->wParam, _eventData->lParam);
+			_eventData->message = WGC_EVENT_PROC_USED;
+		}
+
+		TranslateMessage(_eventData);
+		DispatchMessage(_eventData);
+		return WGC_NO_ERROR;
+	}
+private:
+	MSG* _eventData;
+	std::unordered_map<WGCdlg_h, WGCDialogProc*>* _procMap;
+};
+
+class WGCDialog
+{
+public:
+	WGCDialog(WGCcstr contextName) :
+		_contextName(contextName)
+	{
+		_handle = CreateWindowEx(0U, _contextName, _T("WGC Dialog"), WS_VISIBLE, 0, 0, 100, 100, NULL, 0, CORE::GetAppHandle(), NULL);
+	}
+	WGCDialog(WGCcstr contextName, WGCapp_h appHandle) : 
+		_contextName(contextName)
+	{
+		_handle = CreateWindowEx(0U, _contextName, _T("WGC Dialog"), WS_VISIBLE, 0, 0, 100, 100, NULL, 0, appHandle, NULL);
+	}
+	WGCDialog(WGCcstr contextName, WGCapp_h appHandle, int xPos, int yPos, int width, int height, WGCcstr title) :
+		_contextName(contextName)
+	{
+		_handle = CreateWindowEx(0U, _contextName, title, WS_VISIBLE, xPos, yPos, width, height, NULL, 0, appHandle, NULL);
+	}
+	WGCDialog(WGCcstr contextName, WGCapp_h appHandle, int xPos, int yPos, int width, int height, WGCcstr title, WGCFlag64 basicStyle, WGCFlag64 exStyle, WGCdlg_h hParentDlg, int idValue) :
+		_contextName(contextName)
+	{
+		_handle = CreateWindowEx(exStyle, _contextName, title, basicStyle, xPos, yPos, width, height, hParentDlg, WGC_MAKE_DLG_ID(idValue), appHandle, NULL);
+	}
+	~WGCDialog()
+	{
+		DestroyWindow(this->_handle);
+		this->_handle = NULL;
+	}
+
+	WGCdlg_h get_handle()
+	{
+		return this->_handle;
+	}
+	bool handle_is_valid()
+	{
+		return _handle != NULL;
+	}
+	int start()
+	{
+		if (_handle == NULL)
+			return WGC_ERROR;
+
+		ShowWindow(_handle, SW_SHOW);
+		UpdateWindow(_handle);
+		return WGC_NO_ERROR;
+	}
+private:
+	WGCdlg_h _handle;
+	WGCcstr _contextName;
+};
 
 
 
